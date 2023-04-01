fb0baa311f2fbefe8c9b2ede52352c00eee0ba68
SIDDHARTH JAIN <siddharth1998@gmail.com>
2023-04-01 13:23:39 -0400

    winning condition is working

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 "# stevens-project1-515-adventure" 
 
 
+# Classes
+## Main_Engine
+* The main class where the engine is coded
+* This contains all the funcionalities
+
+## inventory
+* contains all the inventory operations and storage
+
 ---
 
 ---
+
+
 # Functions 
 
 ### __play_game
@@ -19,3 +29,22 @@
 * The output of this function will be used in the validation of the action commands
 * Output is a list of the strings which is required for regex 
 * <h2> if you need to add a new command please add in the respective list </h2>
+
+---
+# Registering a new action
+
+if you want to register a new function you will need to add it into either of the list :
+* no_input_action : if no parameter is required for the action ( parameter are attributes which are required for the action )
+* attribute_input_action: if a parameter is required for the action you will need to add into this list
+
+This will create the custom regex and be registerd in the help also ( This is like the table which the Professor asked for )
+
+then you will need to register the function of the <b> input_parser()</b>
+
+Now if you have no attribute based action then the format will be 
+<code>
+
+elif re.search(self.regex_no_input_action_dict["action_name"], findall_list_user_input[0][0]):
+
+</code>
+
--- adventure.py
+++ adventure.py
@@ -40,11 +40,6 @@ class Main_Engine(object):
         except Exception as e:
             self.printer("Please check the map file")
             return False
-        # string manipulation for exits into lowercase for all character
-        # for i in self.map_of_game_list:
-        #     temp_dict={}
-        #     for z in list(i["exits"]):
-        #         temp_dict[z[0].lower()]=z[1]
         return True
 
     # Printer based system
@@ -68,8 +63,14 @@ class Main_Engine(object):
             return ("Items: "+", ".join(temp_list)+"\n")
         else:
             return
+        
+    def __boss_text_beaut_str(self):
+        return("\n End Level "+self.map_of_game_list[self.which_room_index].get("boss")+"\n")
+
 
     def room_context_printing(self):
+        if self.map_of_game_list[self.which_room_index].get("boss"):
+            self.printer(self.__boss_text_beaut_str())
         self.printer(self.__name_beaut_str())
         self.printer(self._desc_beaut_str())
         x = self.__items__beaut_str()
@@ -83,6 +84,9 @@ class Main_Engine(object):
                 sys.stdout.write(c)
                 sys.stdout.flush()
                 time.sleep(3./90)
+            # for c in str + '\n':
+            #     sys.stdout.write(c)
+            #     sys.stdout.flush()
             # self.printer(str)
             return True
         except:
@@ -105,20 +109,176 @@ class Main_Engine(object):
 
     def __global_action_regex_creator(self):
         # New action command needed to added into this
-        no_input_action = ["look", "inventory", "quit"]
-        attribute_input_action = ["go", "get"]
+        self.no_input_action = ["look", "inventory", "quit","help"]
+        self.attribute_input_action = ["go", "get","drop"]
+        
 
         regex_no_input_action_list = self.__regex_creator_for_actions(
-            no_input_action)
+            self.no_input_action)
         regex_attribute_input_action_list = self.__regex_creator_for_actions(
-            attribute_input_action)
+            self.attribute_input_action)
 
         # ziping the two no_input_action and regex_no_input_action so that we can get dictionary of values
         self.regex_no_input_action_dict = dict(
-            zip(no_input_action, regex_no_input_action_list))
+            zip(self.no_input_action, regex_no_input_action_list))
 
         self.regex_attribute_input_action_dict = dict(
-            zip(attribute_input_action, regex_attribute_input_action_list))
+            zip(self.attribute_input_action, regex_attribute_input_action_list))
+        
+
+    ### Helper function 
+    def is_boss_room(self):
+        if self.map_of_game_list[self.which_room_index].get("boss"): #helper function for winning condition
+            return True 
+        else:
+            return False
+        
+    def what_is_required_for_winning(self):# helper function for the winning but to check if the user has the required items are not
+        winning_condition=self.map_of_game_list[self.which_room_index]["winning"]
+        for  i in winning_condition:
+            if i in self.obj_inventory.inside_inventory:
+                pass
+            else:
+                self.player_loose()
+                return False # will not reach here as self.player_loose will exit if loose
+        self.player_win_banner()
+        return True # will not reach here as self.player_win_banner will exit if won
+        
+    
+    def win_function(self):
+        if self.is_boss_room():
+            if self.what_is_required_for_winning():
+                return True
+            else: 
+                return False
+        else:
+            return False
+    
+    def player_win_banner(self):
+        #  Thanks to for ascii art https://www.asciiart.eu/people/occupations/kings
+        print(r'''
+        
+                                                               o .,<>., o
+                                                               |\/\/\/\/|
+                                                               '========'
+                                                               (_ SSSSSSs
+                                                               )a'`SSSSSs
+                                                              /_   SSSSSS
+                                                              .=## SSSSS
+                                                              .####  SSSSs
+                                                              ###::::SSSSS
+                                                             .;:::""""SSS
+                                                            .:;:'  . .  \\
+                                                           .::/  '     .'|
+                                                          .::( .         |
+                                                          :::)           \
+                                                          /\(            /
+                                                         /)            ( |
+                                                       .'  \  .       ./ /
+                                                    _-'    |\  .        |
+                                  _..--..   .  /"---\      | ` |      . |
+          -=====================,' _     \=(*#(7.#####()   |  `/_..   , (
+                      _.-''``';'-''-) ,.  \ '  '+/// |   .'/   \  ``-.) \
+                    ,'  _.-  ((    `-'  `._\    `` \_/_.'  )    /`-._  ) |
+                  ,'\ ,'  _.'.`:-.    \.-'                 /   <_L   )"  |
+                _/   `._,' ,')`;  `-'`'                    |     L  /    /
+               / `.   ,' ,|_/ / \                          (    <_-'     \
+               \ / `./  '  / /,' \                        /|`         `. |
+               )\   /`._   ,'`._.-\                       |)            \'
+              /  `.'    )-'.-,' )__)                      |\            `|
+             : /`. `.._(--.`':`':/ \                      ) \             \
+             |::::\     ,'/::;-))  /                      ( )`.            |
+             ||:::::  . .::':  :`-(                       |/    .          |
+             ||::::|  . :|  |==[]=:                       .        -       \
+             |||:::|  : ||  :  |  |                      /\           `     |
+ ___ ___     '|;:::|  | |'   \=[]=|                     /  \                \
+|   /_  ||``|||:::::  | ;    | |  |                     \_.'\_               `-.
+:   \_``[]--[]|::::'\_;'     )-'..`._                 .-'\``:: ` .              \
+ \___.>`''-.||:.__,'     SSt |_______`>              <_____:::.         . . \  _/
+                                                           `+a:f:......jrei"""
+ __     __                            _   _                     _                              
+ \ \   / /                           | | | |                   (_)                             
+  \ \_/ /__  _   _    __ _ _ __ ___  | |_| |__   ___  __      ___ _ __  _ __  _ __   ___ _ __  
+   \   / _ \| | | |  / _` | '__/ _ \ | __| '_ \ / _ \ \ \ /\ / / | '_ \| '_ \| '_ \ / _ \ '__| 
+    | | (_) | |_| | | (_| | | |  __/ | |_| | | |  __/  \ V  V /| | | | | | | | | | |  __/ |    
+    |_|\___/ \__,_|  \__,_|_|  \___|  \__|_| |_|\___|   \_/\_/ |_|_| |_|_| |_|_| |_|\___|_|    
+                                                                                               
+      
+        
+        ''')
+        self.printer(self.map_of_game_list[self.which_room_index]["winning_text"])
+        self.printer("******************** THE END ********************")
+        exit(0)
+
+
+
+    
+    def player_loose(self):# helper function for loosing if the player dose not have item he will loose will be called in win_function()
+       # banner from https://ascii.co.uk/art/skulls and https://onlineasciitools.com/convert-text-to-ascii-art
+        print(r''' 
+       ~~~~~~~~~
+     /           \
+    /             \
+   | )           ( |
+    \  /C\   /C\  /
+    /  ~~~   ~~~  \
+    \___  .^,  ___/
+     `| _______ |'
+  _   | HHHHHHH |   _
+ ( )  \         /  ( )
+(_) \  ~~~~^~~~~ ,/ (_)
+  ~\ "\         /  /~
+     \  \     /  /
+       \  \v/  /
+        >     <
+       /  /^\  \
+     /  /     \  \
+ _~/ "/         \  \~_
+( ) /             \ ( )
+ (_)               (_)
+
+  __     ______  _    _   _      ____   ____   _____ ______ 
+ \ \   / / __ \| |  | | | |    / __ \ / __ \ / ____|  ____|
+  \ \_/ / |  | | |  | | | |   | |  | | |  | | (___ | |__   
+   \   /| |  | | |  | | | |   | |  | | |  | |\___ \|  __|  
+    | | | |__| | |__| | | |___| |__| | |__| |____) | |____ 
+    |_|  \____/ \____/  |______\____/ \____/|_____/|______|
+
+        ''')
+        print("Please Play again")
+        exit(0)
+        
+
+   
+
+    #### Operations 
+     
+    def __action_drop(self,get_item):
+        if self.obj_inventory.inside_inventory!=[]:
+            return self.is_item_in_inventory(get_item)
+            # return False     
+        else:
+            print("You're not carrying anything.")# Custom error 
+            return False # TODO handle this
+
+    def is_item_in_inventory(self,get_item):
+        for i in self.obj_inventory.inside_inventory:
+                if i==get_item.lower().strip():# checking the input is in the inventory
+                    temp_data=self.obj_inventory.inside_inventory.pop(self.obj_inventory.inside_inventory.index(i))
+                    # print(temp_data)
+                    if self.map_of_game_list[self.which_room_index].get("items"):
+                        self.map_of_game_list[self.which_room_index]["items"].append(temp_data)
+                    else:
+                        self.map_of_game_list[self.which_room_index]["items"]=[]
+                        self.map_of_game_list[self.which_room_index]["items"].append(temp_data)
+                    return True # if found return True
+        return False # if not found return False
+
+    def __action_help(self):
+        for i in self.attribute_input_action:
+            print("  "+i+" ...")
+        for i in self.no_input_action:
+            print("  "+i)
 
     # to check if the go is possible
     def __go_action_is_it_possible(self, attribute):
@@ -154,15 +314,14 @@ class Main_Engine(object):
                     self.map_of_game_list[self.which_room_index]["items"].pop(self.map_of_game_list[self.which_room_index]["items"].index(item_name))
                     return True
 
-    
 
     def __action_get(self,get_item):# get item action function :: 1
         
         if "items" in self.map_of_game_list[self.which_room_index]:
             
-            if self.__get_me_items(item_name=get_item.lower()):# is item there 
+            if self.__get_me_items(item_name=get_item.lower().strip()):# is item there 
                 temp_item=get_item
-                get_item=get_item.lower()
+                get_item=get_item.lower().strip()
                 self.update_map("pop",get_item)  # if there remove it from map
                 self.add_item(get_item)# and add it to the inventory
                 self.printer(self.printer(f"You pick up the {temp_item}"))
@@ -181,7 +340,7 @@ class Main_Engine(object):
             return False
         else:
             for i in temp_list:
-                if i.lower().strip()==item_name.strip():
+                if i.lower().strip()==item_name:
                     return True
             return False
         
@@ -220,7 +379,10 @@ class Main_Engine(object):
                 
             elif re.search(self.regex_no_input_action_dict["quit"], findall_list_user_input[0][0]):
                 self.__action_quit()
-                pass
+                
+            elif re.search(self.regex_no_input_action_dict["help"], findall_list_user_input[0][0]):
+                self.__action_help()
+                return False
             else:
                 # default
                 # throw error no action like this
@@ -233,6 +395,17 @@ class Main_Engine(object):
                     # TODO ERROR custom errror
                     self.printer("Sorry, you need to 'get' something.")
                     return False
+                if re.search(self.regex_attribute_input_action_dict["drop"], findall_list_user_input[0][0]):
+                    # TODO ERROR custom errror
+                    if len(self.obj_inventory.inside_inventory)==[]:
+                        self.printer("Sorry, you need to have something in inventory to drop something.")
+                        return False
+                    elif len(self.obj_inventory.inside_inventory)==[]:
+                        self.printer("You have one item in inventory please specify the name")
+                    else:
+                        self.printer("Sorry, you need to give an item to drop")
+                    # self.printer("Sorry, you need to have something in inventory to drop something.")
+                    return False
                 else:
                     self.printer("Error, Please give a correct input")
                 return False
@@ -240,8 +413,7 @@ class Main_Engine(object):
         # now to check those actions which will take arguments
         elif findall_list_user_input[0][0] == '' and findall_list_user_input[0][1] != '' and findall_list_user_input[0][2] != '':
             # To remove all white spaces from the input
-            self.parameters_after_action = findall_list_user_input[0][2].strip(
-            )
+            self.parameters_after_action = findall_list_user_input[0][2].strip()
             if re.search(self.regex_attribute_input_action_dict["get"], findall_list_user_input[0][1]):
                 # link the function function go
                 if (self.__action_get(findall_list_user_input[0][2])):
@@ -252,10 +424,15 @@ class Main_Engine(object):
 
             elif re.search(self.regex_attribute_input_action_dict["go"], findall_list_user_input[0][1]):
                 return self.__action_go(self.parameters_after_action)
+            elif re.search(self.regex_attribute_input_action_dict["drop"], findall_list_user_input[0][1]):
+                # this will cotain function calling drop 
+                if self.__action_drop(self.parameters_after_action):
+                    self.printer(f"You drop the {findall_list_user_input[0][2]}.")
+                    return False
             else:
                 # write function to give error for action
                 return False
-            return True
+           
 
         elif findall_list_user_input[0][0] == '' and findall_list_user_input[0][1] != '' and findall_list_user_input[0][2] == '':
             if re.search(self.regex_attribute_input_action_dict["get"], findall_list_user_input[0][1]):
@@ -265,7 +442,6 @@ class Main_Engine(object):
             elif re.search(self.regex_attribute_input_action_dict["go"], findall_list_user_input[0][1]):
                 # TODO ERROR custom
                 self.printer("Sorry, you need to 'go' somewhere.")
-                pass
             else:
                 # generic error the action dose not exsists
                 return False
@@ -275,9 +451,11 @@ class Main_Engine(object):
     # Main game engine
     def __play_game(self):
         # starting_room = self.map_of_game_list[self.which_room_index]
+        
         try:
             while (True):
                 self.room_context_printing()
+                self.win_function()
                 flag = False
                 while (True and not flag):
                     try:
